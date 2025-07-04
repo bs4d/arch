cb0d39474475f140146436aa919aa68f4ead6c69
Bartosz Sadecki <bartosz.sadecki@posteo.pl>
2025-07-04 18:04:00 +0200

    update the scripts post testing

--- install.sh
+++ install.sh
@@ -56,14 +56,14 @@ EOF
 
 # set up LVM with 100% of PV as root
 pvcreate /dev/mapper/lvm
-vgcreate archtmp /dev/mapper/lvm
-lvcreate --extents 100%FREE archtmp --name root
+vgcreate arch /dev/mapper/lvm
+lvcreate --extents 100%FREE arch --name root
 
 # format the root filesystem
-mkfs.ext4 /dev/mapper/archtmp-root
+mkfs.ext4 /dev/mapper/arch-root
 
 # mount it
-mount /dev/mapper/archtmp-root /mnt
+mount /dev/mapper/arch-root /mnt
 
 # wipe lost+found
 rm --recursive --force /mnt/lost+found
@@ -72,7 +72,7 @@ rm --recursive --force /mnt/lost+found
 mount --mkdir "${DISK}1" /mnt/boot
 
 # install basic packages on the target system
-pacstrap -K /mnt base linux linux-firmware lvm2 neovim networkmanager dhcpcd openssh doas grub efibootmgr intel-ucode man-db man-pages texinfo ansible
+pacstrap -K /mnt base linux linux-firmware lvm2 networkmanager dhcpcd doas grub efibootmgr intel-ucode man-db man-pages texinfo ansible
 
 # generate the fstab
 genfstab -U /mnt >> /mnt/etc/fstab
